2887de62d612825ea8acae492d56d57dd3d157ab
Luke Baring <baringluke35@gmail.com>
2026-09-08 20:53:09 -0700

    Add: buy-and-sell-crypto - submission-8

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-8.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min_price = prices[0], max_profit = 0, p = prices.size();
+        for (int i = 1; i < p; i++) {
+            max_profit = max(max_profit, prices[i] - min_price);
+            min_price = min(min_price, prices[i]);
+        }
+        return max_profit;
+    }
+};
